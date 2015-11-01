0eba96152c7c94ead1b00a96421b5d735a2bea04
Omar Abou Mrad <omar.aboumrad@gmail.com>
2015-11-01 11:59:38 +0200

    multiple changes to stats generator
    dropped unused statements
    added argument parser with outfile/author arguments
    restructured code and moved some comments to docstrings
    fixed git command bug and adjusted parser (extra whitespace)
    added simple doctest
    applied a few changes to make the code more pythonic/pep8 friendly

--- git-punch.py
+++ git-punch.py
@@ -1,68 +1,110 @@
 #!/usr/bin/python
 
-import sys
+import argparse
 import subprocess
 import json
 
-# Default config variables
-author = ''
-output_file_name = 'tmp/data.json'
 
-# Function that executes the git
-# commands on a given repository
-# and retrieves the git log data
-def get_log_data():
+def get_log_data(author):
+	"""
+	Function that executes the git commands on a given repository
+	and retrieves the git log data
+	"""
+	command = ['git', 'log',
+			   '--no-merges',
+			   '--author={}'.format(author),
+			   '--pretty=format:%aD']
 	try:
-		gitcommand = ['git', 'log', '--no-merges', '--author='+author,  '--pretty=format: %aD']
-		p = subprocess.Popen(gitcommand,
+		p = subprocess.Popen(command,
 		                     stdin=subprocess.PIPE,
 		                     stdout=subprocess.PIPE,
-		                     stderr=subprocess.PIPE)
+		                     stderr=subprocess.PIPE,
+		                     universal_newlines=True)
 		outdata, errdata = p.communicate()
-	except OSError as e:
+	except OSError:
 		print('Git not installed?')
-		sys.exit(-1)
-	if outdata == '':
+		exit(-1)
+
+	if outdata == '' and errdata:
 		print('Not a git repository?')
-		sys.exit(-1)
+		exit(-1)
+
 	return outdata
 
-def write_to_file(data):
+
+def write_to_file(data, target):
+	"""
+	Writes data to target file
+	"""
 	if data:
 		try:
-			with open(output_file_name, 'w') as outfile:
+			with open(target, 'w') as outfile:
 				json.dump(data, outfile)
-				print("Completed writing data to: %s", output_file_name)
-		except:
+				print("Completed writing data to: {}".format(target))
+		except IOError:
 			print('Could not write data to file')
-			sys.exit(-1)
+			exit(-1)
 
-# Days and Hours listing
-days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-hours = ['12am'] + [str(x) for x in xrange(1, 12)] + ['12pm'] + [str(x) for x in xrange(1, 12)]
 
-# Cleanup the git log data
-temp_log = get_log_data()
-data_log = [[x.strip().split(',')[0], x.strip().split(' ')[4].split(':')[0]] for x in temp_log.split('\n')]
+def build_stats(raw_data):
+	"""
+	Builds statistics table using day of week and hour of day.
 
-# Generates a list of all the
-# days when a commit happened
-# and includes the number of
-# commits at a given hour
-# of the day
-stats = {}
-for d in days:
-	stats[d] = {}
-	for h in xrange(0, 24):
-		stats[d][h] = 0
+	>>> result = build_stats('''Tue, 1 Nov 2015 12:38:23 +0300
+	... Tue, 1 Nov 2015 12:32:04 +0300
+	... Mon, 1 Nov 2015 02:32:04 +0300''')
+	>>> assert result['Tue'][12] == 2
+	>>> assert result['Mon'][2] == 1
+	"""
+	days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 
-# Calculates the total sum
-# of overall commits for the
-# project
-total = 0
-for line in data_log:
-	stats[ line[0] ][ int(line[1]) ] += 1
-	total += 1
+	log = [(x.split(',')[0], x.split(' ')[4].split(':')[0])
+		   for x in raw_data.splitlines()]
 
-# Write data to file
-write_to_file(stats)
+	# Generates a list of all the days when a commit happened
+	# and includes the number of commits at a given hour of the day
+	stats = {}
+	for day in days:
+		stats[day] = {}
+		for hour in xrange(0, 24):
+			stats[day][hour] = 0
+
+	for day, hour in log:
+		stats[day][int(hour)] += 1
+
+	return stats
+
+
+def build_parser():
+	"""
+	Builds the argument parser
+
+	usage: git-punch.py [-h] [--author AUTHOR] [--out-file OUTFILE]
+
+	optional arguments:
+	  -h, --help			show this help message and exit
+	  --author AUTHOR, -a AUTHOR
+							filters based on author
+	  --out-file OUTFILE, -o OUTFILE
+							target output file
+	"""
+	parser = argparse.ArgumentParser()
+
+	parser.add_argument('--author', '-a',
+						dest='author',
+						default='',
+						help='filters based on author')
+
+	parser.add_argument('--out-file', '-o',
+						dest='outfile',
+						default='stats.json',
+						help='target output file')
+
+	return parser
+
+
+if __name__ == '__main__':
+	args = build_parser().parse_args()
+	raw_log = get_log_data(args.author)
+	stats = build_stats(raw_log)
+	write_to_file(stats, args.outfile)
